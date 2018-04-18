31be6b5698e817f5a9d1037ddc364d7d39e6e884
jxmeng <jxmeng@leinao.ai>
2018-04-18 16:24:10 +0800

    add iotest

---
+++ tests/IOTest/README.md
@@ -0,0 +1,52 @@
+#利用Fio工具进行IO测试
+[fio 工具](https://media.readthedocs.org/pdf/fio/latest/fio.pdf)可以测试文件系统读写的 bw, iops, latency
+本脚本利用Fio命令进行文件系统IO的自动化测试
+## 测试步骤
+- 执行脚本  storage_performance_test.sh
+```
+storage_performance_test.sh -d scriptdir -o outputdir
+```
+执行 `storage_performance_test.sh -h` 可以查看脚本执行帮助
+
+```
+storage_performane_test.sh options
+-d,--dir       director containing .fio script
+-o,--output    files produced by fio command will
+               be written into this director
+```
+其中 `-d` 表示fio配置脚本所在的目录, `-o`表示将fio命令生成的结果写入的目录
+脚本会依次执行`scriptdir`目录中的所有配置脚本
+- fio配置脚本介绍
+在script_sample目录中有示例,详细介绍请参见[fio pdf文档](https://media.readthedocs.org/pdf/fio/latest/fio.pdf)
+
+```
+[global]
+#ioengine 表示执行时所采用的IO机制
+ioengine=libaio
+#direct=1表示不采用linux系统的缓冲机制
+direct=1
+#块大小为4k
+bs=4k
+#文件大小为512M
+size=512m
+#directory=/home/ustc/test
+#任务数为10个
+numjobs=10
+#将多个任务的输出报告合并为一个
+group_reporting=1
+#指定任务的运行时间
+runtime=120
+#指定IO队列深度
+iodepth=32
+#任务名称
+[job_sequential_randread]
+#测试时生成的文件路径和名称
+filename=/home/ustc/test/test_rw_randread
+#测试的读写模式:read,write, randread, randwrite, randrw
+rw=randread
+```
+`filename=/home/ustc/test/test_rw_randread`filename中指定的目录所在文件系统即为要测试的文件系统
+**如若要测试某个NFS系统,需要先将NFS挂载到本地目录如/home/ustc/test/,然后针对/home/ustc/test/进行fio测试**
+
+- 输出结果含义
+可以参考 [Fio Output Explained](http://tobert.github.io/post/2014-04-17-fio-output-explained.html)
---
+++ tests/IOTest/storage_performance_test.sh
@@ -0,0 +1,171 @@
+#!/bin/bash
+script_ext='.fio'
+#set -x
+arg=$(getopt -o d:o:h -l dir,output:,help -- $@)
+#echo "${arg}"
+eval set -- "${arg}"
+
+function runecho()
+{
+    echo ""
+    echo $@
+}
+
+while true
+do
+    case ${1} in 
+        -d|--dir)
+            ScriptDir=$2
+            runecho ScriptDir:$ScriptDir
+            shift 2
+            ;;
+        -o|--output)
+            output=$2
+            runecho output:$output
+            shift 2
+            ;;
+        -h|--help)
+            cat <<EOF
+            storage_performane_test.sh options
+            -d,--dir       director containing .fio script 
+            -o,--output    files produced by fio command will 
+                           be written into this director
+EOF
+            shift
+            exit 0
+            ;;
+        --)
+            shift
+            break
+            ;;
+    esac
+done
+
+#${1} name of parameter 
+#${2} value of parameter
+function checkpara()
+{
+    if [ -z ${2} ]
+    then
+        runecho ${1} not exist, please check
+        exit 1
+    fi
+}
+
+# remove last / of path
+# example: /home/test/ ===> /home/test
+function filePathTrim()
+{
+    if [ -z ${1} ]
+    then
+        runecho please input file path
+        exit 1
+    fi
+
+    lastChar=${1:(-1)}
+
+    if [ "${lastChar}" == '/' ]
+    then
+        output=${1%/}
+    else
+        output=${1}
+    fi
+}
+
+checkpara "ScriptDir" ${ScriptDir}
+checkpara "output"    ${output}
+
+:<<zhushi
+if [ "$ScriptDir"_ == "_" ]
+then
+    runecho please input script dir
+    exit 1
+fi
+
+if [ -z $output ]
+then
+    runecho please input output director
+    exit 1
+fi
+zhushi
+
+# step 1: detect the tool: fio
+
+fio -h >/dev/null
+if [ $? -eq 0 ]
+then
+   runecho fio installed already
+else
+   echo ubuntu | sudo -S apt-get -y install fio
+
+   if [ $? -ne 0 ]
+   then
+      echo !!!!!!!!!!!something wrong with installing fio, please check.!!!!!!!!!!!!
+      exit 1
+   fi
+fi 
+
+# step 2: make output director
+mkdir -p ${output}
+
+# step 3: fio test
+filePathTrim ${output}
+echo output:${output}
+
+ScriptDir=${ScriptDir%/}
+echo "ScriptDir:${ScriptDir}"
+
+for file in $(ls ${ScriptDir} | grep ${script_ext})
+do
+    filebase=$(basename ${file})
+    runecho "execute command : fio $ScriptDir/${file} output:${output}/""${filebase%.*}_result.txt"
+    fio "$ScriptDir/${file}" | tee ${output}/"${filebase%.*}_result.txt"
+done
+
+# get path of temp file
+function getFilePathInScipt()
+{
+    scriptPath=${1}
+    if [ "${scriptPath}_" == "_" ]
+    then
+        runecho usage: getFilePathInScipt scriptPath
+        runecho please check the code
+        exit 1
+    fi
+
+    if [ ! -f ${scriptPath} ]
+    then
+        runecho file ${scriptPath} does not exist
+        return 0
+    fi
+
+    filePath=$(cat ${scriptPath} | grep filename)
+    #echo $filePath
+    filePath=${filePath#*=}
+    #echo $filePath
+    runecho filepath in function :$filePath
+    
+    if [ ! -z ${2} ]
+    then
+        eval ${2}=${filePath}
+    fi
+}
+
+# remove temp file
+for file in $(ls ${ScriptDir} | grep ${script_ext})
+do
+    filebase=$(basename ${file})
+    runecho "execute command : getFilePathInScipt  $ScriptDir/${file} "
+    getFilePathInScipt "$ScriptDir/${file}" filePathOut
+    #echo filepath in loop :$filePath
+    runecho filePathOut : $filePathOut
+    if [ -f $filePathOut ]
+    then
+        runecho rmoving the file: $filePathOut
+        rm -f $filePathOut
+    fi
+done
+
+runecho all fio task finished
+echo -e --------please check the files about fio result in: "\033[34m ${output} \033[0m"--------
+
