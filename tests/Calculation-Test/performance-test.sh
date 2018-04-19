bb64da0bf2a22298698f17ce152238d1906777ee
flyliu2017 <flyliu.mail.ustc.edu.cn>
2018-04-19 15:09:05 +0800

    calculation test

---
+++ tests/Calculation-Test/performance-test.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+#Test calculation performance and bandwidth of all gpus (one by one) on the machine.
+#The result will be saved in test-result.txt
+
+nvidia-smi -L
+
+gpunum=$(nvidia-smi -L|wc -l)
+
+i=0
+while [ $i -lt $gpunum ]
+do
+    ./batchCUBLAS -N100 -m1536 -n1536 -k1536 device=$i
+    echo -e "\n"
+    ./bandwidthTest --device=$i
+    let "i++"
+done | tee test-result.txt
+
